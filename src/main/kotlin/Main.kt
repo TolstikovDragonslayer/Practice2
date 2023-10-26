afd3546651bff4bbc7263eae217204023c60318a
TolstikovDragonslayer <123370348+TolstikovDragonslayer@users.noreply.github.com>
2023-10-26 08:14:40 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+fun main(args: Array<String>) {
+    //first exercise
+    val age1 = 42
+    val age2 = 21
+
+    //second exercise
+    val avg1 = ((age1 + age2) / 2)
+
+    // third exercise
+
+    //fourth exercise
+    val firstName = "Vlad"
+    val lastName = "Tolstikov"
+
+    //fifth exercise
+    val fullName = firstName + " " + lastName
+
+    //sixth exercise
+    val myDetails = "Hi, my name is $fullName"
+
+    //seventh exercise
+    val Triple = Triple(17,2,2005)
+
+    //eight exercise
+    val day = Triple.first
+    val month = Triple.second
+    val year = Triple.third
+
+    //ninth exercise
+    println(month)
+    println(year)
+
+    //tenth exercise
+    val newMonth = Triple.second + 3
+    val Pair = Pair(newMonth, Triple.third)
+
+}
